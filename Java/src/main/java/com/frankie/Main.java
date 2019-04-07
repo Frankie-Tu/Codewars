73807c126c86524b6498f94fc1e5671a2c165afe
Frankie Tu <hua.tu@hotmail.com>
2019-04-07 15:00:33 -0400

    restructured project to a Maven project, removed one Python challenge, added new Java challenge RectangleToSquares

---
+++ Java/src/main/java/com/frankie/Main.java
@@ -0,0 +1,9 @@
+    package com.frankie;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(RectangleToSquares.sqInRect(5,3));
+    }
+
+}
---
+++ Java/src/main/java/com/frankie/RectangleToSquares.java
@@ -0,0 +1,40 @@
+package com.frankie;
+
+import java.util.ArrayList;
+/*
+Credit to g964, creator of this original Codewars challenge
+given rectangle with length lng and width wdth.
+Find the combination of squares that fill the rectangle
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(RectangleToSquares.sqInRect(5,3));
+    }
+
+}
+ */
+
+
+public class RectangleToSquares {
+
+    public static ArrayList<Integer> sqInRect(int lng, int wdth) {
+        ArrayList<Integer> resultList = new ArrayList<Integer>();
+        if (lng == wdth){
+            return null;
+        }
+        while (true) {
+            if (lng > wdth) {
+                resultList.add(wdth);
+                lng -= wdth;
+            } else if (lng < wdth) {
+                resultList.add(lng);
+                wdth -= lng;
+            } else {
+                resultList.add(lng);
+                break;
+            }
+        }
+        return resultList;
+    }
+}
